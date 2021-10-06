431fc5b078c94a925b051aea8ab435d23ccda19a
Ori Tabac <ori.tabac@whitesourcesoftware.com>
2021-10-06 06:55:33 +0300

    add WS_URL parameter to documentation

--- pipelineSBOM.sh
+++ pipelineSBOM.sh
@@ -8,7 +8,8 @@
 # Add the following after calling the unified agent in any pipeline file to create an SPDX output from the scanned project to the whitesource logs folder
 # then use your pipeline publish feature to save the whitesource log folder as an artifact as shown in the README
 #         curl -LJO https://raw.githubusercontent.com/whitesource-ft/ws-examples/main/pipelineSBOM.sh
-#         chmod +x ./pipelineSBOM.sh && ./pipelineSBOM.sh
+#         chmod +x ./pipelineSBOM.sh && ./pipelineSBOM.sh WS_URL
+#         URL options: saas, saas-eu, app, app-eu
 
 # More information & usage regarding WS SBOM generator can be found at https://github.com/whitesource-ps/ws-sbom-spdx-report
 
