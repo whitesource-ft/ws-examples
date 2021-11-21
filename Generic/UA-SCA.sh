7fa81c6eec24e6e9673cb40754c4b3b45262aa97
samq-ws <sam.quakenbush@whitesourcesoftware.com>
2021-11-21 07:11:56 -0700

    multiple fixes

--- Generic/UA-SCA.sh
+++ Generic/UA-SCA.sh
@@ -5,6 +5,7 @@ export WS_APIKEY=<your-api-key>
 export WS_USERKEY=<your-user-key>
 export WS_PRODUCTNAME=<your-product-name>
 export WS_PROJECTNAME=<your-project-name>
+export WS_WSS_URL=https://saas.whitesourcesoftware.com/agent
 curl -LJO https://unified-agent.s3.amazonaws.com/wss-unified-agent.jar
 echo Unified Agent downloaded successfully
-java -jar wss-unified-agent.jar -d ./
+java -jar wss-unified-agent.jar
