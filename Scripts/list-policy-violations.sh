e9f0e43975bf3ce83c06f06583ac1d21c33cee46
tidharm <tidhar.meltzer@whitesourcesoftware.com>
2022-02-03 17:24:12 +0200

    script cleanup

--- Scripts/list-policy-violations.sh
+++ Scripts/list-policy-violations.sh
@@ -41,38 +41,3 @@ cat $jsonFile | jq -c '.rejectingPolicies[]' | while read oPolicy; do
         echo ""
     done
 done
-
-
-
-
-
-
-
-
-
-
-declare -a policyRejectionSummary=( $(cat $jsonFile | jq -r '.rejectingPolicies[] | (.policyName + "," + .filterType + "," + .rejectedLibraries)') )
-
-for project in "${projects[@]}"; do
-    IFS=, read projectToken projectName <<< "$project"
-    printf "\nWhiteSource Vulnerability Alerts for project: ${BL}%s${NC}\n" "$projectName"
-    apiRes="$(curl -s -X POST -H "Content-Type: application/json" -d '{ "requestType": "getProjectAlertsByType", "alertType": "SECURITY_VULNERABILITY", "userKey": "'"$WS_USERKEY"'", "projectToken": '"$projectToken"' }' $WS_API_URL)"
-
-    # High severity CVEs
-    cveH="$(echo "$apiRes" | jq -r '.alerts[] | select(.vulnerability.cvss3_severity=="high") | ("[H] " + .vulnerability.name + " - " + .library.filename)')"
-    cveCountH="$([ "${#cveH}" -gt 0 ] && echo "$cveH" | wc -l || echo 0)"
-
-    # Medium severity CVEs
-    cveM="$(echo "$apiRes" | jq -r '.alerts[] | select(.vulnerability.cvss3_severity=="medium") | ("[M] " + .vulnerability.name + " - " + .library.filename)')"
-    cveCountM="$([ "${#cveM}" -gt 0 ] && echo "$cveM" | wc -l || echo 0)"
-
-    # Low severity CVEs
-    cveL="$(echo "$apiRes" | jq -r '.alerts[] | select(.vulnerability.cvss3_severity=="low") | ("[L] " + .vulnerability.name + " - " + .library.filename)')"
-    cveCountL="$([ "${#cveL}" -gt 0 ] && echo "$cveL" | wc -l || echo 0)"
-
-    printf "Alerts: ${RD}$cveCountH High${NC}, ${YW}$cveCountM Medium${NC}, ${GN}$cveCountL Low${NC}\n\n"
-    printf "${RD}$cveH${NC}\n"
-    printf "${YW}$cveM${NC}\n"
-    printf "${GN}$cveL${NC}\n"
-    printf "\n"
-done
