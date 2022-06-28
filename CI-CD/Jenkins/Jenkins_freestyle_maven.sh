f1a530284003686553d82089fe9aeda50f44853f
Luke Brogan <87319782+lukebroganws@users.noreply.github.com>
2022-06-28 08:10:53 +0100

    Update Jenkins_freestyle_maven.sh

--- CI-CD/Jenkins/Jenkins_freestyle_maven.sh
+++ CI-CD/Jenkins/Jenkins_freestyle_maven.sh
@@ -1,4 +1,4 @@
-echo "Downloading WS"
+echo "Downloading Mend"
 if ! [ -f ./wss-unified-agent.jar ]; then
   curl -fSL -R -JO https://unified-agent.s3.amazonaws.com/wss-unified-agent.jar
   if [[ "$(curl -sL https://unified-agent.s3.amazonaws.com/wss-unified-agent.jar.sha256)" != "$(sha256sum wss-unified-agent.jar)" ]]; then
@@ -6,7 +6,7 @@ if ! [ -f ./wss-unified-agent.jar ]; then
     exit -7
   fi
 fi
-echo "Exceute WS"
+echo "Exceute Mend"
 export WS_APIKEY=${APIKEY} #Taken from Jenkins Global Environment Variables
 export WS_USERKEY=${USERKEY} #Taken from Jenkins Global Environment Variables
 export WS_WSS_URL="https://saas.whitesourcesoftware.com/agent"
