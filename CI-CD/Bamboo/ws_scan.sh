24fc9cc6ee67e59e097fc329a4e14d7293c9f40d
tabac-ws <84838519+tabac-ws@users.noreply.github.com>
2022-07-05 10:45:33 +0300

    Gitlab+bamboo ua integrity (#110)
    * Fix Mend CLI step in Jenkins pipeline
    
    * Revert "Fix Mend CLI step in Jenkins pipeline"
    
    This reverts commit 3f61cdf1d235755c91ee2bc946a34068b64bccf3.
    
    * add ua integrity checks to bamboo and gitlab
    
    Co-authored-by: tabac-ws <ori.tabac@whitesourcesoftware.com”git config --global user.name Ori>

--- CI-CD/Bamboo/ws_scan.sh
+++ CI-CD/Bamboo/ws_scan.sh
@@ -7,7 +7,15 @@
 # WS_USERKEY = {MASKED_USERKEY}
 
 # Download Unified Agent
+echo Downloading WhiteSource Unified Agent
 curl -LJO https://unified-agent.s3.amazonaws.com/wss-unified-agent.jar
+if [[ "$(curl -sL https://unified-agent.s3.amazonaws.com/wss-unified-agent.jar.sha256)" != "$(sha256sum wss-unified-agent.jar)" ]] ; then
+    echo "Integrity Check Failed"
+else
+    echo "Integrity Check Passed"
+    echo "Starting WhiteSource Scan"
+    java -jar wss-unified-agent.jar
+fi
 
 # Scan with Unified Agent
 java -jar wss-unified-agent.jar 
